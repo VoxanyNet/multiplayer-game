cfdf71823c1f18578f80ceb0a858e5d1270866d5
VoxanyNet <contact@voxany.net>
2023-07-23 22:20:46 -0400

    started work on TileEntity

---
+++ src/engine/tileentity.py
@@ -0,0 +1,21 @@
+from typing import Union, List, TYPE_CHECKING
+
+from pygame import Rect
+
+from engine.entity import Entity
+from engine.tile import Tile    
+
+if TYPE_CHECKING:
+    from engine.gamemode_client import GamemodeClient
+    from engine.gamemode_server import GamemodeServer
+
+class TileEntity(Entity):
+    """An entity that is composed of physics tiles"""
+    def __init__(self, interaction_rect: Rect, game: Union["GamemodeClient", "GamemodeServer"], updater: str, id: str,
+                 visible=True):
+        
+        self.visible = visible
+
+        self.tiles: List[Tile] = []
+        
+        super().__init__(interaction_rect=interaction_rect, game=game, updater=updater, id=id)
