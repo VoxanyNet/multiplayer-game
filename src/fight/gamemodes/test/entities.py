f09c22430b12c89679953d3870e5aa20c8ea8ee5
VoxanyNet <contact@voxany.net>
2023-07-25 16:45:53 -0400

    delete all test entity methods

--- src/fight/gamemodes/test/entities.py
+++ src/fight/gamemodes/test/entities.py
@@ -1,23 +1,17 @@
 from typing import Union, Tuple
 
 from pygame import Rect
+import pymunk
 
 from engine.entity import Entity
 from engine.gamemode_client import GamemodeClient
 from engine.gamemode_server import GamemodeServer
 from engine.tile import Tile
+from engine.tileentity import TileEntity
 
-class TestDynamic(Entity):
-    def __init__(self, interaction_rect: Rect, game: Union["GamemodeClient", "GamemodeServer"], updater: str, id: str,
-                 visible=True):
-        
-        super().__init__(interaction_rect=interaction_rect, game=game, updater=updater, id=id, visible=visible)
+class TestDynamic(TileEntity):
+    
+    pass 
+class TestStatic(TileEntity):
 
-class TestStatic(Entity):
-
-    def __init__(self, interaction_rect: Rect, game: Union["GamemodeClient", "GamemodeServer"], updater: str, id: str,
-                 visible=True):
-        
-        super().__init__(interaction_rect=interaction_rect, game=game, updater=updater, id=id, visible=visible)
-
-        tile = Tile(self, position=(500, 500), velocity=(0,0))
+    pass
