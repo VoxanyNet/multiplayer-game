7298163514fb3859445ed5f63bd1aae93dbf100f
VoxanyNet <contact@voxany.net>
2023-07-25 21:21:04 -0400

    added server_port parameter

--- src/fight/gamemodes/test/server.py
+++ src/fight/gamemodes/test/server.py
@@ -9,8 +9,8 @@ from engine.events import ServerStart
 from fight.gamemodes.test.entities import TestDynamic
 
 class TestServer(GamemodeServer):
-    def __init__(self, tick_rate, server_ip: str = socket.gethostname()):
-        super().__init__(tick_rate=tick_rate, server_ip=server_ip)
+    def __init__(self, tick_rate, server_ip: str = socket.gethostname(), server_port: int = 5560):
+        super().__init__(tick_rate=tick_rate, server_ip=server_ip, server_port=server_port)
 
         self.entity_type_map.update(
             {
