15514c81022ff39d45fcb5896975edfa6505d692
VoxanyNet <contact@voxany.net>
2023-08-16 19:27:20 -0400

    updated autoconnect script

--- src/fight/autoconnect.py
+++ src/fight/autoconnect.py
@@ -1,5 +1,5 @@
-from fight.gamemodes.test.client import TestClient
+from fight.gamemodes.ragdoll.client import RagdollClient
 
-game = TestClient(server_ip="voxany.net", server_port=25580, network_compression=True)
+game = RagdollClient(server_ip="voxany.net", server_port=25580, network_compression=True)
 
-game.run(max_tick_rate=-1,network_tick_rate=30)
+game.run(max_tick_rate=-1,network_tick_rate=60)
