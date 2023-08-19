a417f1abc71c98fda1e1297548636ead1b36a23b
VoxanyNet <contact@voxany.net>
2023-08-19 14:56:38 -0400

    improved example entity comments

--- src/engine/example_entity.py
+++ src/engine/example_entity.py
@@ -17,14 +17,23 @@ class ExampleEntity(Entity):
     def serialize(self) -> Dict[str, int | bool | str | list]:
         data_dict = super().serialize()
         # serialize entity data to be json compatible
-        # data_dict["velocity"] = [self.velocity.x, self.velocity.y]
+        # data_dict.update(
+        #   {
+        #       "level": self.level
+        #   }
+        # )
+        return data_dict
     
     def update(self, update_data):
         super().update(update_data)
         # update entity attributes with update_data
         # for example:
-        # self.velocity.x = entity_data["velocity"][0]
-        # self.velocity.y = entity_data["velocity"][1]
+        # for attribute_name, attribute_value in update_data.items():
+
+        #     match attribute_name:
+                
+        #         case "level":
+        #             self.level = update_data["level"]
     
     @classmethod
     def create(cls, entity_data: Dict[str, int | bool | str | list], entity_id: str, game: type[GamemodeClient] | type[GamemodeServer]) -> type[Entity]:
