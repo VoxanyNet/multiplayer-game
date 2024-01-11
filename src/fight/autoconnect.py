8503ad728c6d3252eafa1c61e2d9f6aec627bae0
VoxanyNet <contact@voxany.net>
2024-01-11 01:44:47 -0500

    moved fight game to seperate repo

--- src/fight/autoconnect.py
+++
@@ -1,5 +0,0 @@
-from fight.gamemodes.test.client import Client
-
-game = Client(server_ip="voxany.net", server_port=25580, network_compression=True)
-
-game.run(max_tick_rate=-1,network_tick_rate=30)
